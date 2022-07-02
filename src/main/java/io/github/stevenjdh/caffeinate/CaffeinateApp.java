0f9c1c2a7c002694d0dbb835cc9489b0c40b1c03
Steven Jenkins De Haro <20492442+StevenJDH@users.noreply.github.com>
2022-07-03 01:12:14 +0200

    Added console logo

--- src/main/java/io/github/stevenjdh/caffeinate/CaffeinateApp.java
+++ src/main/java/io/github/stevenjdh/caffeinate/CaffeinateApp.java
@@ -33,10 +33,12 @@ public class CaffeinateApp {
 
     private static final Logger LOG = LoggerFactory.getLogger(CaffeinateApp.class.getName());
 
+    @SuppressWarnings("java:S106")
     public static void main(String[] args) throws AWTException {        
         var executor = Executors.newSingleThreadScheduledExecutor();
         var robot = new Robot();
-
+        
+        System.out.println(getLogo());
         GlobalScreen.addNativeKeyListener(new DecaffeinateNativeKeyListener(executor));
         LOG.info("Caffeinating...");
 
@@ -44,5 +46,19 @@ public class CaffeinateApp {
             Point location = MouseInfo.getPointerInfo().getLocation();
             robot.mouseMove(location.x, location.y); // Sets to same location to make invisible to user.
         }, 5, 5, TimeUnit.SECONDS);
+    }
+    
+    private static String getLogo() {
+        var sb = new StringBuilder();
+        
+        sb.append("MM'\"\"\"\"'YMM          .8888b .8888b          oo                     dP\n");            
+        sb.append("M' .mmm. `M          88   \" 88   \"                                 88\n");            
+        sb.append("M  MMMMMooM .d8888b. 88aaa  88aaa  .d8888b. dP 88d888b. .d8888b. d8888P .d8888b.\n"); 
+        sb.append("M  MMMMMMMM 88'  `88 88     88     88ooood8 88 88'  `88 88'  `88   88   88ooood8\n"); 
+        sb.append("M. `MMM' .M 88.  .88 88     88     88.  ... 88 88    88 88.  .88   88   88.  ...\n"); 
+        sb.append("MM.     .dM `88888P8 dP     dP     `88888P' dP dP    dP `88888P8   dP   `88888P'\n"); 
+        sb.append("MMMMMMMMMMM                                       Steven Jenkins De Haro v1.0.0\n");
+        
+        return sb.toString();
     }
 }
